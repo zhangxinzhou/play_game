490375d4f8f48b50081f87a71bbac8346075126a
zhangxinzhou <zhangxinzhou@github.com>
2021-07-25 14:19:54 +0800

    aaa

--- faptitans_script/auto_reborn3.py
+++ faptitans_script/auto_reborn3.py
@@ -1,7 +1,11 @@
 # encoding=utf-8
 
 import time
+import datetime
 import pyautogui
+
+# 关闭安全模式
+pyautogui.FAILSAFE
 
 # =====坐标=====
 # 基准xy(偏移量 )
@@ -22,6 +26,9 @@ hero_top02_xy = (hero_base_xy[0] + hero_offset_x, hero_base_xy[1])
 hero_top03_xy = (hero_base_xy[0], hero_base_xy[1] + hero_offset_y)
 hero_top04_xy = (hero_base_xy[0] + hero_offset_x, hero_base_xy[1] + hero_offset_y)
 hero_list = [hero_top01_xy, hero_top02_xy, hero_top03_xy, hero_top04_xy]
+
+# 再现boss
+boss_xy = (858, 442)
 
 # 技能坐标
 skill_base_xy = (545, 140)
@@ -108,33 +115,50 @@ def handle_shade_cover():
 def handle_skill():
     img = get_img()
     skill01_ok = img_position_match_color(img, skill01_xy, skill_ok_color)
-    skill07_ok = img_position_match_color(img, skill07_xy, skill_ok_color)
+    skill04_ok = img_position_match_color(img, skill04_xy, skill_ok_color)
     skill_xy_list = []
-    if skill01_ok and not skill07_ok:
+    if skill01_ok and not skill04_ok:
         # 点技能1
         skill_xy_list = [skill01_xy]
-    elif skill01_ok and skill07_ok:
+    elif skill01_ok and skill04_ok:
         # 点技能2,3,4,5,6,7,1
-        skill_xy_list = [skill02_xy, skill03_xy, skill04_xy, skill05_xy, skill06_xy, skill07_xy, skill01_xy]
+        skill_xy_list = [boss_xy, skill02_xy, skill03_xy, skill04_xy, skill05_xy, skill06_xy, skill07_xy, skill01_xy]
+
+    # 执行操作
     for skill_xy in skill_xy_list:
         click_xy = get_click_xy(skill_xy)
         click_xy = (click_xy[0] + 10, click_xy[1] + 10)
-        pyautogui.click(click_xy)
+        pyautogui.click(click_xy, duration=0.1)
 
 
 # 升级英雄
-def hero_level_up():
+def hero_level_up1():
+    # 依次判断四个英雄是否可以购买或者升级
+    max_times = 10
+    while max_times > 0:
+        max_times -= 1
+        img = get_img()
+        for hero_xy in hero_list:
+            if img_position_match_color(img, hero_xy, hero_ok_color):
+                click_xy = get_click_xy(hero_xy)
+                pyautogui.click(click_xy)
+                time.sleep(1.2)
+                break
+
+
+# 升级英雄
+def play_game():
     # 如果有遮罩层,先处理遮罩层
     # handle_shade_cover()
-    # 依次判断四个英雄是否可以购买或者升级
-    for hero_xy in hero_list:
-        if img_position_match_color(get_img(), hero_xy, hero_ok_color):
-            click_xy = get_click_xy(hero_xy)
-            pyautogui.click(click_xy)
-    # 休眠两秒,等待按钮颜色恢复正常
-    time.sleep(2)
+    # 使用技能
+    handle_skill()
+    # 英雄升级
+    hero_level_up1()
 
 
 if __name__ == '__main__':
     pyautogui.screenshot('screen.png', img_region)
-    handle_skill()
+    while True:
+        play_game()
+        print(datetime.datetime.now())
+        time.sleep(10)
