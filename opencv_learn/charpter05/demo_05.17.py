b341dcb613214d6ef6feb87dd07f5c52bd5eade9
zhangxinzhou <zhangxinzhou@github.com>
2021-08-10 23:25:22 +0800

    123

---
+++ opencv_learn/charpter05/demo_05.17.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+img = cv2.imread(r"..\lena.jpg")
+rows, cols = img.shape[:2]
+mapx = np.zeros(img.shape[:2], np.float32)
+mapy = np.zeros(img.shape[:2], np.float32)
+for i in range(rows):
+    for j in range(cols):
+        mapx.itemset((i, j), cols - 1 - j)
+        mapy.itemset((i, j), rows - 1 - i)
+rst = cv2.remap(img, mapx, mapy, cv2.INTER_LINEAR)
+cv2.imshow("original", img)
+cv2.imshow("result", rst)
+cv2.waitKey()
+cv2.destroyAllWindows()
---
+++ opencv_learn/charpter05/demo_05.18.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+img = np.random.randint(0, 256, size=[4, 5], dtype=np.uint8)
+rows, cols = img.shape[:2]
+mapx = np.zeros(img.shape[:2], np.float32)
+mapy = np.zeros(img.shape[:2], np.float32)
+for i in range(rows):
+    for j in range(cols):
+        mapx.itemset((i, j), i)
+        mapy.itemset((i, j), j)
+rst = cv2.remap(img, mapx, mapy, cv2.INTER_LINEAR)
+
+print("img=\n", img)
+print("mapx=\n", mapx)
+print("mapy=\n", mapy)
+print("rst=\n", rst)
