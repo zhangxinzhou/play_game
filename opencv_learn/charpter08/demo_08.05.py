1db7d656c1aafe7119e10abf119e012286d98ba9
zhangxinzhou <zhangxinzhou@github.com>
2021-08-18 00:06:39 +0800

    123

---
+++ opencv_learn/charpter08/demo_08.05.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+
+o = cv2.imread(r"..\lena.jpg", cv2.IMREAD_UNCHANGED)
+kernel = np.ones((9, 9), np.uint8)
+dilation = cv2.dilate(o, kernel)
+cv2.imshow("original", o)
+cv2.imshow("dilation", dilation)
+cv2.waitKey()
+cv2.destroyAllWindows()
