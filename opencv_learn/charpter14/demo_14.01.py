b3119f477c306f5f4f1c5a3b16a07a860187e265
zhangxinzhou <zhangxinzhou@github.com>
2021-09-08 19:46:41 +0800

    2021-09-06~2021-09-07

--- opencv_learn/charpter14/demo_14.01.py
+++ opencv_learn/charpter14/demo_14.01.py
@@ -2,18 +2,28 @@ import cv2
 import numpy as np
 import matplotlib.pylab as plt
 
-img = cv2.imread(r"..\lena.jpg", 0)
+original_img = cv2.imread(r"..\lena.jpg", 0)
 # 傅里叶变换,图像=>负数数组(频谱信息)
-f = np.fft.fft2(img)
+original_fft = np.fft.fft2(original_img)
 # 原始频谱中的零频率分量移动到频域图像的中心位置
-fshift = np.fft.fftshift(f)
-magnitude_spectrum = 20 * np.log(np.abs(fshift))
-plt.subplot(1, 2, 1)
-plt.imshow(img, cmap='gray')
+original_fft_shift = np.fft.fftshift(original_fft)
+
+original_fft_ = 20 * np.log(np.abs(original_fft))
+original_fft_shift_ = 20 * np.log(np.abs(original_fft_shift))
+
+plt.subplot(1, 3, 1)
+plt.imshow(original_img, cmap='gray')
 plt.title("original")
 plt.axis("off")
-plt.subplot(1, 2, 2)
-plt.imshow(magnitude_spectrum, cmap='gray')
-plt.title("result")
+
+plt.subplot(1, 3, 2)
+plt.imshow(original_fft_, cmap='gray')
+plt.title("original_fft_")
 plt.axis("off")
+
+plt.subplot(1, 3, 3)
+plt.imshow(original_fft_shift_, cmap='gray')
+plt.title("original_fft_shift_")
+plt.axis("off")
+
 plt.show()
