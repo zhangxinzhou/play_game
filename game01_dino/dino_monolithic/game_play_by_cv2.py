ddf087be4529b9890a46520d1a6bc83977ae354d
zhangxinzhou <xiaoxinerror@live.cn>
2021-10-21 22:47:50 +0800

    2021-10-20

--- game01_dino/dino_monolithic/game_play_by_cv2.py
+++ game01_dino/dino_monolithic/game_play_by_cv2.py
@@ -7,6 +7,7 @@ import pyautogui
 import cv2
 from PyQt5.QtWidgets import QApplication
 from public_utils import qpixmap_to_array
+from numba import jit
 
 """
 比较满意的截图方式,速度很快,
@@ -41,57 +42,52 @@ app = QApplication(sys.argv)
 screen = app.primaryScreen()
 
 
-# 随机行动测试
-def random_action():
-    action = random.randint(0, 2)
-    print(action)
-    if action == 1:
+# 行动,如果前方有障碍物就跳起来
+def action_cv2(image_tmp):
+    has_obstacle = np.mean(image_tmp[140:160, 115:155]) > 50
+    has_bird = np.mean(image_tmp[80:110, 120:160]) > 50
+    if has_obstacle:
         pyautogui.press('up')
-    elif action == 2:
+    elif has_bird:
         pyautogui.press('down')
 
 
-# 截图
-img_name = 'buff.jpg'
+@jit()
+def game_over(image_tmp):
+    return np.mean(image_tmp[100:130, 230:270]) > 80
+
+
+t_total_start = time.time()
+pyautogui.press('up')
 while True:
-    t_total_start = time.time()
+    t_cycle_start = time.time()
 
     t_grab_start = time.time()
     q_pix_map = screen.grabWindow(0, new_pos[0], new_pos[1], new_pos[2], new_pos[3])
-    t_grab_end = time.time()
-    t_grab_cost = (t_grab_end - t_grab_start) * 1000
-
-    t_load_start = time.time()
-    q_pix_map.save('buff.jpg')
-    mat1 = cv2.imread(img_name)
-    t_load_end = time.time()
-    t_load_cost = (t_load_end - t_load_start) * 1000
+    t_grab_cost = (time.time() - t_grab_start) * 1000
 
     t_transform_start = time.time()
-    mat2 = qpixmap_to_array.qpixmap_to_array(q_pix_map)
-    mat2 = cv2.cvtColor(mat2, cv2.COLOR_BGR2GRAY)
-    t_transform_end = time.time()
-    t_transform_cost = (t_transform_end - t_transform_start) * 1000
+    img = qpixmap_to_array.qpixmap_to_array(q_pix_map)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    action_cv2(img)
+    t_transform_cost = (time.time() - t_transform_start) * 1000
 
     t_show_start = time.time()
-    cv2.imshow('mat1', mat1)
-    cv2.imshow('mat2', mat2)
+    cv2.imshow('img', img)
     cv2.waitKey(1)
-    t_show_end = time.time()
-    t_show_cost = (t_show_end - t_show_start) * 1000
+    t_show_cost = (time.time() - t_show_start) * 1000
 
-    is_game_over = np.mean(mat2[100:130, 230:270]) > 80
-    print("is game over : {}".format(is_game_over))
-    if is_game_over:
-        exit(0)
-
-    t_total_end = time.time()
-    t_total_cost = (t_total_end - t_total_start) * 1000
-    text = "size: [{}], grab cost: [{:.4f}] ms, load cost: [{:.4f}] ms, transform cost: [{:.4f}] ms, show cost:[{:.4f}] ms, total cost:[{:.4f}] ms".format(
+    t_cycle_cost = (time.time() - t_cycle_start) * 1000
+    t_total_cost = (time.time() - t_total_start) * 1000
+    text = "size=[{}], grab=[{:.2f}]ms, transform=[{:.2f}]ms, show=[{:.4f}]ms, cycle=[{:.2f}]ms, total cost:[{:.2f}]ms".format(
         q_pix_map.size(),
         t_grab_cost,
-        t_load_cost,
         t_transform_cost,
         t_show_cost,
+        t_cycle_cost,
         t_total_cost)
     print(text)
+
+    if game_over(img):
+        print("game is over!!!")
+        # exit(0)
