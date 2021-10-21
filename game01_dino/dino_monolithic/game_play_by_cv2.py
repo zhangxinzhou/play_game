2987c8f4913cda2c7702091d6d2d46bcf7548f98
zhangxinzhou <xiaoxinerror@live.cn>
2021-10-21 23:15:29 +0800

    2021-10-21

--- game01_dino/dino_monolithic/game_play_by_cv2.py
+++ game01_dino/dino_monolithic/game_play_by_cv2.py
@@ -43,13 +43,16 @@ screen = app.primaryScreen()
 
 
 # 行动,如果前方有障碍物就跳起来
+@jit()
 def action_cv2(image_tmp):
     has_obstacle = np.mean(image_tmp[140:160, 115:155]) > 50
     has_bird = np.mean(image_tmp[80:110, 120:160]) > 50
+    action = 0
     if has_obstacle:
-        pyautogui.press('up')
+        action = 1
     elif has_bird:
-        pyautogui.press('down')
+        action = 2
+    return action
 
 
 @jit()
@@ -57,37 +60,23 @@ def game_over(image_tmp):
     return np.mean(image_tmp[100:130, 230:270]) > 80
 
 
-t_total_start = time.time()
+time.sleep(1)
 pyautogui.press('up')
 while True:
     t_cycle_start = time.time()
 
-    t_grab_start = time.time()
     q_pix_map = screen.grabWindow(0, new_pos[0], new_pos[1], new_pos[2], new_pos[3])
-    t_grab_cost = (time.time() - t_grab_start) * 1000
 
-    t_transform_start = time.time()
     img = qpixmap_to_array.qpixmap_to_array(q_pix_map)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    action_cv2(img)
-    t_transform_cost = (time.time() - t_transform_start) * 1000
-
-    t_show_start = time.time()
-    cv2.imshow('img', img)
-    cv2.waitKey(1)
-    t_show_cost = (time.time() - t_show_start) * 1000
+    action = action_cv2(img)
+    if action == 1:
+        pyautogui.press('up')
+    elif action == 2:
+        pyautogui.press('down')
 
     t_cycle_cost = (time.time() - t_cycle_start) * 1000
-    t_total_cost = (time.time() - t_total_start) * 1000
-    text = "size=[{}], grab=[{:.2f}]ms, transform=[{:.2f}]ms, show=[{:.4f}]ms, cycle=[{:.2f}]ms, total cost:[{:.2f}]ms".format(
-        q_pix_map.size(),
-        t_grab_cost,
-        t_transform_cost,
-        t_show_cost,
-        t_cycle_cost,
-        t_total_cost)
-    print(text)
-
+    print("action :[{}], cost : [{:.2f}]".format(action, t_cycle_cost))
     if game_over(img):
         print("game is over!!!")
-        # exit(0)
+        exit(0)
