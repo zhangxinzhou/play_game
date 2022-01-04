bf83c7191c4241024e344edfdbd9a775c228c7a7
zhangxinzhou <xiaoxinerror@live.cn>
2022-01-04 22:18:51 +0800

    2022-01-04

--- rl_learn/Q-Learning.py
+++ rl_learn/Q-Learning.py
@@ -8,7 +8,7 @@ parser.add_argument('--test', dest='test', action='store_true', default=True)
 args = parser.parse_args()
 
 
-class Sarsa:
+class QLearning:
     def __init__(self, state_dim, action_dim, lr=0.01, gamma=0.9, e_greed=0.1):
         self.action_dim = action_dim
         self.lr = lr
@@ -25,26 +25,24 @@ class Sarsa:
 
     def predict(self, state):
         """ 根据输入的观察值,预测输出的动作值 """
-        all_actions = self.Q[state, :]
-        max_action = np.max(all_actions)
-        # 防止最大的Q值有多个,找出所有最大的Q,然后再随机选择
-        # where函数返回一个array,每个元素为下标
-        max_action_list = np.where(all_actions == max_action)[0]
+        all_action = self.Q[state, :]
+        max_action = np.max(all_action)
+        max_action_list = np.where(all_action == max_action)[0]
         action = np.random.choice(max_action_list)
         return action
 
-    def learn(self, state, action, reward, next_state, next_action, done):
+    def learn(self, state, action, reward, next_state, done):
         if done:
             target_q = reward
         else:
-            target_q = reward + self.gamma * self.Q[next_state, next_action]
+            target_q = reward + self.gamma * np.max(self.Q[next_state, :])
         self.Q[state, action] += self.lr * (target_q - self.Q[state, action])
 
-    def save(self, npy_file='./model/sarsa_q_table.npy'):
+    def save(self, npy_file='./model/qlearning_table.npy'):
         np.save(npy_file, self.Q)
         print(npy_file + ' saved.')
 
-    def load(self, npy_file='./model/sarsa_q_table.npy'):
+    def load(self, npy_file='./model/qlearning_table.npy'):
         self.Q = np.load(npy_file)
         print(npy_file + ' loaded.')
 
@@ -55,16 +53,14 @@ class Agent:
         self.lr = 0.1
         self.gamma = 0.9
         self.e_greed = 0.1
-        # type(self.state_dim)L <class 'tuple'>
-        # type(self.observation_space.n): <class 'int'>
-        self.model = Sarsa(env.observation_space.n, env.action_space.n)
+        self.model = QLearning(env.observation_space.n, env.action_space.n)
 
     def train(self, max_episode):
         if args.train:
             for episode in range(max_episode):
                 ep_reward, ep_steps = self.run_episode(render=False)
                 if episode % 20 == 0:
-                    print("Episode %03s: steps = %02s, reward = %.1f" % (episode, ep_steps, ep_reward))
+                    print("Episode %03s: stpes = %02s,reward=%0.1f" % (episode, ep_steps, ep_reward))
             self.model.save()
 
         if args.test:
@@ -75,23 +71,18 @@ class Agent:
         total_reward = 0
         total_steps = 0
         state = self.env.reset()
-        action = self.model.sample(state)
         while True:
+            action = self.model.sample(state)
             next_state, reward, done, _ = self.env.step(action)
-            next_action = self.model.sample(next_state)
-            # 训练Q-learning算法
-            self.model.learn(state, action, reward, next_state, next_action, done)
+            self.model.learn(state, action, reward, next_state, done)
 
             state = next_state
-            action = next_action
             total_reward += reward
             total_steps += 1
-
             if render:
                 self.env.render()
             if done:
                 break
-
         return total_reward, total_steps
 
     def test_episode(self, render=False):
@@ -108,13 +99,12 @@ class Agent:
                 self.env.render()
             if done:
                 break
-
         print('test reward = %.1f' % total_reward)
-        print('test actions is:', actions)
+        print('test action is: ', action)
 
 
 if __name__ == '__main__':
-    # 使用gym创建迷宫环境,设置is_slippery为False降低环境难度
+    # 使用gym创建宫环境,设置is_slippery为false降级环境难度
     env = gym.make('FrozenLake-v1', is_slippery=False)
 
     agent = Agent(env=env)
