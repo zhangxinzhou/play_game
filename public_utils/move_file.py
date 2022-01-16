7e6f7dbb1037caddb7012f31d7e759390de506af
zhangxinzhou <xiaoxinerror@live.cn>
2022-01-16 23:07:50 +0800

    2022-01-04

---
+++ public_utils/move_file.py
@@ -0,0 +1,14 @@
+import os
+import shutil
+
+path = r'E:\BaiduNetdiskDownload\Mantis-X'
+
+for root, dirs, files in os.walk(path):
+    print(f"root={root},dirs={dirs},files={files}")
+    for file in files:
+        src = os.path.join(root, file)
+        dst = os.path.join(path, file)
+        print(f"********************** file={src}")
+        if not os.path.exists(dst):
+            print(f"start move {dst}")
+            shutil.move(src, path)
