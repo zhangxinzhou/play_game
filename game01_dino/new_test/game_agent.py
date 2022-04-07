9822ed712fa5031f7daf7d9682eefa7a48de25ff
zhangxinzhou <xiaoxinerror@live.cn>
2022-04-07 23:07:41 +0800

    123

--- game01_dino/new_test/game_agent.py
+++ game01_dino/new_test/game_agent.py
@@ -2,4 +2,27 @@ from abc import abstractmethod
 
 
 class GameAgent(object):
-    pass
+
+    @abstractmethod
+    def model_create(self):
+        # 模型创建
+        pass
+
+    @abstractmethod
+    def model_mutation(self):
+        # 模型变异-变异程度由低到高
+        # 1.完全继承/复制-降低lr重训练
+        # 2.完全继承/复制-并重训练
+        # 3.降低5%结构,并重训练
+        # 4.增加5%结构,并重训练
+        pass
+
+    @abstractmethod
+    def model_save(self):
+        # 模型保存
+        pass
+
+    @abstractmethod
+    def model_load(self):
+        # 模型加载
+        pass
