437c8bd6d6c848e686236f8767382cc0507c96fd
zhangxinzhou <xiaoxinerror@live.cn>
2022-04-23 00:21:51 +0800

    123

--- game01_dino/new_test/db_utils.py
+++ game01_dino/new_test/db_utils.py
@@ -10,27 +10,49 @@ host = 'localhost'
 port = '5432'
 
 conn = psycopg2.connect(database=db, user=user, password=pw, host=host, port=port)
-
 curs = conn.cursor()
-
-uid = uuid.uuid4()
-# insert_sql = f"INSERT INTO zxz1(id, name) VALUES('{uid}', '{uid}')"
-# curs.execute(insert_sql)
-# curs.execute('commit')
-
-select_sql = "select * from model_generation"  # 从表格table中读取全表内容
-curs.execute(select_sql)  # 执行该sql语句
-data = curs.fetchall()  # 获取数据
 
 
 def generate_uuid():
-    uid = uuid.uuid4().hex
-    return uid
+    return uuid.uuid4().hex
+
+
+def conn_close():
+    curs.close()
+    conn.close()
+
+
+def insert_model_generation(model_id):
+    sql = f"insert into model_generation (model_id) values('{model_id}')"
+    curs.execute(sql)
+    # 提交事务
+    curs.execute("commit;")
+
+
+def insert_model_train_detail(model_id):
+    sql = f"insert into model_train_detail (model_id) values('{model_id}')"
+    curs.execute(sql)
+    # 提交事务
+    curs.execute("commit;")
+
+
+def query_model_generation_by_model_id(model_id):
+    sql = f"select * from model_generation where model_id = '{model_id}'"
+    curs.execute(sql)
+    return curs.fetchall()
+
+
+def query_model_train_detail_by_model_id(model_id):
+    sql = f"select * from model_train_detail where model_id = '{model_id}'"
+    curs.execute(sql)
+    return curs.fetchall()
 
 
 if __name__ == '__main__':
-    uid = generate_uuid()
-    print(uid)
-
-curs.close()
-conn.close()
+    model_id = generate_uuid()
+    insert_model_generation(model_id)
+    insert_model_train_detail(model_id)
+    d1 = query_model_generation_by_model_id(model_id)
+    d2 = query_model_train_detail_by_model_id(model_id)
+    print(d1)
+    print(d2)
