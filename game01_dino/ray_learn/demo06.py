bd9aa2a5beadc422a404422bf8c0dcb28817e503
zxz <aabbcc5050@qq.com>
2022-07-23 01:02:35 +0800

    123

--- game01_dino/ray_learn/demo06.py
+++ game01_dino/ray_learn/demo06.py
@@ -1,12 +1,15 @@
 import random
 import numpy as np
-import gym, ray
+import gym
 from gym.spaces import Discrete, Box
-from ray.rllib.agents import ppo
+
+
+# import ray
+# from ray.rllib.agents import ppo
 
 
 class MyEnv1(gym.Env):
-    def __init__(self, config: dict):
+    def __init__(self, config: dict = None):
         self.action_space = Discrete(n=2)
         self.observation_space = Box(low=0.0, high=1.0, shape=(1,), dtype=np.float32)
         self.count = 0
@@ -23,7 +26,7 @@ class MyEnv1(gym.Env):
         step_value = random.randint(0, 1)
         observation = np.array([float(step_value)])
         reward: float = 1.0 if action == self.correct_action else 0.0
-        done: bool = self.count >= 100
+        done: bool = self.count >= 10
         info: dict = {}
         self.count += 1
         self.correct_action = step_value
@@ -39,15 +42,18 @@ class MyEnv1(gym.Env):
         pass
 
 
-ray.init()
-trainer = ppo.PPOTrainer(
-    env=MyEnv1,
-    config={
-        "env_config": {
-            "corridor_length": 5
-        }
-    }
-)
-
-while True:
-    print(trainer.train())
+if __name__ == '__main__':
+    env = MyEnv1()
+    for i in range(10):
+        print("=" * 20, str(i), "=" * 20)
+        prev_observation_ = env.reset()
+        while True:
+            action_ = random.randint(0, 1)
+            observation_, reward_, done_, info_ = env.step(action=action_)
+            print(f"observation_={observation_}, "
+                  f"prev_observation_={prev_observation_}, "
+                  f"action_={action_}, "
+                  f"reward_={reward_} ")
+            prev_observation_ = observation_
+            if done_:
+                break
