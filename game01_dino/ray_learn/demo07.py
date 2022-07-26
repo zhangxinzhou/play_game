e4927ee1c73206c52330a13e8901ec335aeb6b61
zxz <aabbcc5050@qq.com>
2022-07-26 23:18:28 +0800

    123

--- game01_dino/ray_learn/demo07.py
+++ game01_dino/ray_learn/demo07.py
@@ -1,6 +1,8 @@
-from demo06 import MyEnv1
-
+import os
 import ray
+import shutil
+
+from demo06 import MyEnv1
 from ray.rllib.agents import ppo
 from ray.tune.logger import pretty_print
 
@@ -10,20 +12,22 @@ ppo_config['model']['fcnet_hiddens'] = [256, 256]
 trainer = ppo.PPOTrainer(env=MyEnv1, config=ppo_config)
 env = MyEnv1()
 episode_reward_best = 0
-checkpoint_dir = r'F:\models\dino'
-for i in range(30):
+model_name = 'aaa'
+checkpoint_dir = fr'F:\models\dino\{model_name}'
+not_increase = 0
+for age in range(30):
     trainer_result = trainer.train()
-    print("*" * 50, i, "*" * 50)
-    print(pretty_print(trainer_result))
-
     episode_reward_max = trainer_result['episode_reward_max']
     episode_reward_min = trainer_result['episode_reward_min']
     episode_reward_mean = trainer_result['episode_reward_mean']
     agent_timesteps_total = trainer_result['agent_timesteps_total']
 
+    # 保存模型
     if episode_reward_mean > episode_reward_best:
-        episode_reward_best = episode_reward_mean
-        print(trainer.save(checkpoint_dir))
+        not_increase = 0
+        if checkpoint_path is not None and os.path.exists(checkpoint_path):
+            shutil.rmtree(os.path.dirname(checkpoint_path))
+        checkpoint_path = trainer.save(checkpoint_dir)
 
-
-
+# 保存数据,进行下一个模型结构的迭代
+# model_save
