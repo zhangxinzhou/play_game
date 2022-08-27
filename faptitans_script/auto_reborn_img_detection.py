a8b9b53a771555c609bc587abf94aa67d9c783c0
zxz <aabbcc5050@qq.com>
2022-08-27 14:47:54 +0800

    Changes

--- faptitans_script/auto_reborn_img_detection.py
+++ faptitans_script/auto_reborn_img_detection.py
@@ -5,10 +5,10 @@ import os
 from datetime import datetime
 
 hero_lv_up = "detection_img/hero_lv_up.png"
+boss = "detection_img/boss.png"
 skill_01 = "detection_img/skill_01.png"
 skill_04 = "detection_img/skill_04.png"
-skill_07 = "detection_img/skill_07.png"
-skill_len = 70
+skill_interval_y_axis = 70
 
 
 def get_close_img_list():
@@ -24,34 +24,52 @@ def get_close_img_list():
 while True:
     print(datetime.now())
     time.sleep(1)
-    # 关闭遮罩层
-    close_list = get_close_img_list()
-    for close_path in close_list:
-        close_location = pyautogui.locateOnScreen(image=close_path)
-        if close_location is not None:
-            x, y = pyautogui.center(close_location)
-            pyautogui.leftClick(x, y)
-
     # 先技能
     hero_lv_up_location = pyautogui.locateOnScreen(image=hero_lv_up)
     sk01_location = pyautogui.locateOnScreen(image=skill_01)
     sk04_location = pyautogui.locateOnScreen(image=skill_04)
-    sk07_location = pyautogui.locateOnScreen(image=skill_07)
-    if sk01_location is not None and sk04_location is not None and sk07_location is not None:
-        x, y = pyautogui.center(sk01_location)
-        for index in range(7):
-            pyautogui.leftClick(x, y + skill_len * index)
 
+    #  技能处理
+    if sk04_location is not None and sk01_location is not None:
+        # 如果技能4ok,技能1ok,则点击技能1~7
+        x, y = pyautogui.center(sk01_location)
+        pyautogui.leftClick(x, y + skill_interval_y_axis * (7 - 1))
+        pyautogui.leftClick(x, y + skill_interval_y_axis * (1 - 1))
+        pyautogui.leftClick(x, y + skill_interval_y_axis * (2 - 1))
+        pyautogui.leftClick(x, y + skill_interval_y_axis * (3 - 1))
+        pyautogui.leftClick(x, y + skill_interval_y_axis * (4 - 1))
+        pyautogui.leftClick(x, y + skill_interval_y_axis * (5 - 1))
+        pyautogui.leftClick(x, y + skill_interval_y_axis * (6 - 1))
+        # 处理boss再现
+        boss_location = pyautogui.locateOnScreen(image=boss)
+        if boss_location is not None:
+            x, y = pyautogui.center(boss_location)
+            pyautogui.leftClick(x, y)
+        continue
+    elif sk04_location is not None and sk01_location is not None:
+        # 如果技能4ok,技能1不ok,则等待技能(什么都不做)
+        pass
+    elif sk01_location is not None:
+        # 如果技能1ok,则点击技能1
+        x, y = pyautogui.center(sk01_location)
+        pyautogui.leftClick(x, y)
+        # 处理boss再现
+        boss_location = pyautogui.locateOnScreen(image=boss)
+        if boss_location is not None:
+            x, y = pyautogui.center(boss_location)
+            pyautogui.leftClick(x, y)
+        continue
+
+    # 处理技能升级
     if hero_lv_up_location is not None:
         x, y = pyautogui.center(hero_lv_up_location)
         pyautogui.leftClick(x, y)
+        continue
 
-    if sk04_location is not None:
-        if sk01_location is None:
-            # 4技能OK,1技能不OK,啥都不干
-            continue
-    else:
-        if sk01_location is not None:
-            # 4技能不OK,1技能OK,使用1技能
-            x, y = pyautogui.center(sk01_location)
+    # 关闭遮罩层
+    close_list = get_close_img_list()
+    for close_path in close_list:
+        close_location = pyautogui.locateOnScreen(image=close_path, confidence=0.9)
+        if close_location is not None:
+            x, y = pyautogui.center(close_location)
             pyautogui.leftClick(x, y)
