c4b6900ad48ab80f4e5e5d964b069c6a63e5cc8d
zxz <xiaoxinerror@live.cn>
2023-08-13 21:17:17 +0800

    111

--- faptitans_script/click_just.py
+++ faptitans_script/click_just.py
@@ -12,6 +12,7 @@ pyautogui.PAUSE = 0.0001
 
 # 开关,控制,如果监听到esc按键被按下,整个程序就会停止
 switch = True
+pause = True
 
 
 def on_press(key):
@@ -24,12 +25,20 @@ def on_press(key):
         print("*" * 50, "程序将在本次训练完毕后结束...", "*" * 50)
         return False
     elif key == keyboard.Key.space:
+        global pause
+        pause = not pause
         print("*" * 50, f"监听到{key}按键被按下...", "*" * 50)
-        print("*" * 50, "开始", "*" * 50)
-        pyautogui.click(clicks=30)
+        msg = "暂停" if pause else "启动"
+        print("*" * 50, msg, "*" * 50)
 
 
-# listener = keyboard.Listener(on_press=on_press)
-# listener.start()
-with keyboard.Listener(keys='', on_press=on_press) as listener:
-    listener.join()
+listener = keyboard.Listener(on_press=on_press)
+listener.start()
+# with keyboard.Listener(keys='', on_press=on_press) as listener:
+#     listener.join()
+
+while switch:
+    if pause:
+        continue
+    pyautogui.click(clicks=30)
+    time.sleep(1)
